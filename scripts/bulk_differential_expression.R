d1b64ad97cd4239ad4367a3f336cb77b855ae549
Jacqueline Marcia Boccacino <jb62@sanger.ac.uk>
2023-11-17 22:56:02 +0000

    Adding scripts used for analyses.

---
+++ scripts/bulk_differential_expression.R
@@ -0,0 +1,357 @@
+# ANALYSIS OF IN-HOUSE BULK RNA-SEQ DATA ######################################
+# @ This script generates plots present in the second and fourth main figures # 
+# @ of the manuscript + supplemental material.                                #
+###############################################################################
+#                                       #
+# Differential gene expression analysis #
+#                                       #
+#########################################
+
+##########################
+#### Loading packages ####
+##########################
+
+# R v4.0.3
+library(DESeq2) # v1.30.1
+library(dplyr) # v1.0.10 
+library(ggplot2) # v3.3.6
+library(RColorBrewer) # v1.1_3
+library(ggrepel) # v0.9.1
+
+
+#################################
+#### Defining some functions ####
+#################################
+
+find_degs <- function(
+     counts, # Count matrix.
+     metadata, # Metadata table.
+     analysis_design, # Design for DESeq2 analysis (e.g. ~genotype).
+     column, # Name of column that contains the conditions to be compared.
+     condition_of_interest, # Name of condition of interest (e.g. TGA).
+     control_condition # Name of control condition (e.g. WT).
+) {
+     # Create DESeq2 object.
+     dds <- DESeqDataSetFromMatrix(
+          countData = counts,
+          colData = metadata,
+          design = analysis_design
+     )
+
+     # Filter out genes that have very low expression across samples.
+     dds <- dds[rowSums(counts(dds)) > 1,]
+
+     # Run differential gene expression analysis.
+     dds <- estimateSizeFactors(dds)
+     dds <- DESeq(dds)
+
+     # Save DESeq2-normalized counts to output file.
+     write.csv(
+          counts(dds, normalized = TRUE), 
+          paste0(getwd(), "/results/bulk/", condition_of_interest, "_vs_", control_condition, "/DESeq2-normalized_counts.csv")
+     )
+
+     # Extract results with the contrast of interest.
+     res_unshrunken <- results(
+          dds, 
+          contrast = c(column, condition_of_interest, control_condition), 
+          alpha = 0.05
+     )
+
+     # Make plot with unshrunken results & save it to output file.
+     pdf(
+          paste0(getwd(), "/results/bulk/", condition_of_interest, "_vs_", control_condition, "/DESeq2_MAplot_unshrunken_results.pdf"),
+          height = 6, 
+          width = 7
+     )
+     plotMA(res_unshrunken)
+     dev.off()
+
+     # Perform log fold change shrinkage.
+     res_shrunken <- lfcShrink(
+          dds = dds, 
+          res = res_unshrunken, 
+          contrast = c(column, condition_of_interest, control_condition), 
+          type = "normal"
+     )
+
+     # Make plot with shrunken results & save it to output file.
+     pdf(
+          paste0(getwd(), "/results/bulk/", condition_of_interest, "_vs_", control_condition, "/DESeq2_MAplot_shrunken_results.pdf"),
+          height = 6, 
+          width = 7
+     )
+     plotMA(res_shrunken)
+     dev.off()
+
+     res_shrunken <- as.data.frame(res_shrunken)
+
+     # Save unfiltered results to output file.
+     write.csv(
+          res_shrunken, 
+          paste0(getwd(), "/results/bulk/", condition_of_interest, "_vs_", control_condition, "/DESeq2_unfiltered_DEGs.csv")
+     )
+
+     # Create dataframe with only significant results.
+     deg_sig <- res_shrunken[res_shrunken$pvalue <= 0.05,]
+     
+     # Save filtered results to output file.
+     write.csv(
+          deg_sig, 
+          paste0(getwd(), "/results/bulk/", condition_of_interest, "_vs_", control_condition, "/DESeq2_filtered_DEGs_pvalue0.05.csv")
+     )
+
+     return(res_shrunken)
+}
+
+make_volcano <- function(
+     degs, # DESeq2 output.
+     lfc_threshold, # Log fold change threshold.
+     genelist, # List of genes to be highlighted.
+     title, # Title for volcano plot.
+     nudge_x_ # Horizontal adjustment for point labels.
+) {
+
+     # Classify genes.
+     degs <- degs %>% mutate(status = case_when(
+          pvalue <= 0.05 & log2FoldChange >= lfc_threshold ~ "Upregulated",
+          pvalue <= 0.05 & log2FoldChange <= -lfc_threshold ~ "Downregulated",
+          pvalue > 0.05 ~ "Non-significant",
+          abs(log2FoldChange) < lfc_threshold ~ "Non-significant",
+     ))
+
+     # Subset genes to be highlighted on the volcano plot.
+     degs_to_highlight <- degs[rownames(degs) %in% genelist,]
+
+     # Create volcano plot.
+     volcano <- ggplot(
+          degs,
+          aes(x = log2FoldChange, y = -log10(pvalue), color = status)
+     ) +
+          geom_point(alpha = 0.5) +
+          theme_bw() +
+          theme(
+               axis.text = element_text(size = 10),
+               axis.title = element_text(size = 12),
+               legend.text = element_text(size = 10),
+               legend.title = element_text(size = 12),
+               plot.title = element_text(size = 12, face = 'bold', hjust = 0.5)
+          ) +
+          scale_color_manual(
+               name = '',
+               values = c('royalblue1', 'snow3', 'indianred1'),
+               labels = c(
+                    paste0('Downregulated (n=', nrow(degs[degs$status == "Downregulated",]), ')'),
+                    paste0('Non-significant (n=', nrow(degs[degs$status == "Non-significant",]), ')'),
+                    paste0('Upregulated (n=', nrow(degs[degs$status == "Upregulated",]), ')')
+               )
+          ) +
+          geom_vline(xintercept = c(-lfc_threshold, lfc_threshold), linetype = 'dashed') +
+          geom_hline(yintercept = -log10(0.05), linetype = 'dashed') +
+          geom_text_repel(
+               data = degs_to_highlight %>% filter(status == "Downregulated"),
+               aes(label = degs_to_highlight %>% filter(status == "Downregulated") %>% rownames),
+               show.legend = FALSE,
+               size = 2,
+               color = 'black',
+               nudge_x = -nudge_x_
+          ) +
+          geom_text_repel(
+               data = degs_to_highlight %>% filter(status == "Upregulated"),
+               aes(label = degs_to_highlight %>% filter(status == "Upregulated") %>% rownames),
+               show.legend = FALSE,
+               size = 2,
+               color = 'black',
+               nudge_x = nudge_x_
+          ) +
+          xlab(expression(log[2]~'(Fold change)')) +
+          ylab(expression(-log[10]~'(p-value)')) +
+          ggtitle(title)
+
+     # Return volcano plot.
+     return(volcano)
+}
+
+
+#########################################
+#### Reading & processing input data ####
+#########################################
+
+#### TGA vs WT ####
+
+# Load counts.
+oe_counts <- read.csv(
+     paste0(getwd(), "/data/TGA/TGA_vs_WT_raw_counts.csv"),
+     header = TRUE,
+     row.names = 1,
+     sep = ";"
+)
+
+# Load metadata.
+oe_metadata <- read.csv(
+     paste0(getwd(), "/data/TGA/TGA_vs_WT_metadata.csv"),
+     header = TRUE,
+     row.names = 1,
+     sep = ";"
+)
+
+# Find differentially expressed genes.
+oe_degs <- find_degs(
+     counts = oe_counts,
+     metadata = oe_metadata,
+     analysis_design = as.formula(paste("~", "Genotype")),
+     column = "Genotype",
+     condition_of_interest = "TGA",
+     control_condition = "WT"
+)
+
+# FIGURE 2C (MAIN)
+# Make volcano plot.
+pdf(
+     paste0(getwd(), "/results/bulk/TGA_vs_WT/DESeq2_volcano_plot.pdf"),
+     width = 5, 
+     height = 3
+)
+make_volcano(
+     degs = oe_degs,
+     lfc_threshold = 0,
+     genelist = c("Stip1", "Lefty2", "Mid1", "Gata6", "Klf2", "Bmp4", "Cdkn1b", "Anxa1", "Hspa12a", "Tfcp2l1"),
+     title = expression(STIP1^'TgA'~italic('versus')~'WT'),
+     nudge_x_ = 0.5
+)
+dev.off()
+
+
+#### HTKO vs WT ####
+
+# Read counts.
+htko_counts <- read.csv(
+     paste0(getwd(), "/data/HTKO/HTKO_vs_WT_raw_counts.csv"),
+     header = TRUE,
+     row.names = 1,
+     sep = ";"
+)
+
+# Read metadata.
+htko_metadata <- read.csv(
+     paste0(getwd(), "/data/HTKO/HTKO_vs_WT_metadata.csv"),
+     header = TRUE,
+     row.names = 1,
+     sep = ";"
+)
+
+# Find differentially expressed genes.
+htko_degs <- find_degs(
+     counts = htko_counts,
+     metadata = htko_metadata,
+     analysis_design = as.formula(paste("~", "Genotype")),
+     column = "Genotype",
+     condition_of_interest = "HTKO",
+     control_condition = "WT"
+)
+
+# FIGURE 2B (MAIN)
+# Make volcano plot.
+pdf(
+     paste0(getwd(), "/results/bulk/HTKO_vs_WT/DESeq2_volcano_plot.pdf"),
+     width = 5, 
+     height = 3
+)
+make_volcano(
+     degs = htko_degs,
+     lfc_threshold = 0,
+     genelist = c("Stip1", "Usp11", "Foxo3", "Hspa1b"),
+     title = expression(STIP1^'+/-'~italic('versus')~'WT'),
+     nudge_x_ = 0
+)
+dev.off()
+
+
+#### TGA vs HTKO ####
+
+# Read counts.
+oe_counts <- read.csv(
+     paste0(getwd(), "/data/TGA/TGA_vs_HTKO_raw_counts.csv"),
+     header = TRUE,
+     row.names = 1,
+     sep = ";"
+)
+
+# Read metadata.
+oe_metadata <- read.csv(
+     paste0(getwd(), "/data/TGA/TGA_vs_HTKO_metadata.csv"),
+     header = TRUE,
+     row.names = 1,
+     sep = ";"
+)
+
+# Find differentially expressed genes.
+oe_degs <- find_degs(
+     counts = oe_counts,
+     metadata = oe_metadata,
+     analysis_design = as.formula(paste("~", "Genotype")),
+     column = "Genotype",
+     condition_of_interest = "TGA",
+     control_condition = "HTKO"
+)
+
+# FIGURE 2H (MAIN)
+# Make volcano plot.
+pdf(
+     paste0(getwd(), "/results/bulk/TGA_vs_HTKO/DESeq2_volcano_plot.pdf"),
+     width = 5, 
+     height = 3
+)
+make_volcano(
+     degs = oe_degs,
+     lfc_threshold = 0,
+     genelist = c("Stip1", "Hspa1b"),
+     title = expression(STIP1^'TgA'~italic('versus')~STIP1^'+/-'),
+     nudge_x_ = 0.5
+)
+dev.off()
+
+
+#### TPR1 vs WT ####
+
+# Read counts.
+delta_counts <- read.csv(
+     paste0(getwd(), "/data/TPR1/TPR1_vs_WT_raw_counts.csv"),
+     header = TRUE,
+     row.names = 1,
+     sep = ";"
+)
+
+# Read metadata.
+delta_metadata <- read.csv(
+     paste0(getwd(), "/data/TPR1/TPR1_vs_WT_metadata.csv"),
+     header = TRUE,
+     row.names = 1,
+     sep = ";"
+)
+
+# Find differentially expressed genes.
+delta_degs <- find_degs(
+     counts = delta_counts,
+     metadata = delta_metadata,
+     analysis_design = as.formula(paste("~", "Genotype")),
+     column = "Genotype",
+     condition_of_interest = "TPR1",
+     control_condition = "WT"
+)
+
+# FIGURE 4C (MAIN)
+# Make volcano plot.
+pdf(
+     paste0(getwd(), "/results/bulk/TPR1_vs_WT/DESeq2_volcano_plot.pdf"),
+     width = 5, 
+     height = 3
+)
+make_volcano(
+     degs = delta_degs,
+     lfc_threshold = 0,
+     genelist = c("Stip1", "Lefty1", "Lefty2", "Klf2", "Foxo4", "Otx2", "Gata4", "Gata6", "Fgfr4", "Hspa1a", "Hspa1b"),
+     title = expression(STIP1^{Delta~'TPR1'}~italic('versus')~'WT'),
+     nudge_x_ = 0.5
+)
+dev.off()
---
+++ scripts/bulk_enrichment_analysis.R
@@ -0,0 +1,301 @@
+# ANALYSIS OF IN-HOUSE BULK RNA-SEQ DATA ######################################
+# @ This script generates plots present in the second and fourth main figures # 
+# @ of the manuscript + supplemental material.                                #
+###############################################################################
+#                      #
+# Functional profiling #
+#                      #
+########################
+
+
+##########################
+#### Loading packages ####
+##########################
+
+# R v4.0.3
+library(clusterProfiler) # v3.18.1
+library(org.Mm.eg.db) # v3.12.0
+library(ggplot2) # v3.3.6
+library(dplyr) # v1.0.10 
+
+
+#################################
+#### Defining some functions ####
+#################################
+
+process_degs <- function(
+     deg_df, # Dataframe containing DEGs as output by DESeq2.
+     comparison # Conditions that are being compared.
+) {
+     # Create ranked gene list.
+     ranked_genelist <- deg_df$log2FoldChange
+     names(ranked_genelist) <- deg_df$X
+     ranked_genelist <- sort(ranked_genelist, decreasing = TRUE)
+     
+     # Create gene lists for Over-Representation Analysis (ORA).
+     genelist_pos <- names(ranked_genelist)[ranked_genelist > 0]
+     genelist_neg <- names(ranked_genelist)[ranked_genelist < 0]
+     genelist <- list(
+          "Upregulated" = genelist_pos,
+          "Downregulated" = genelist_neg
+     )
+
+     # Perform ORA using the Gene Ontology (GO) database.
+     go_res <- compareCluster(
+          genelist,
+          fun = "enrichGO",
+          ont = "ALL",
+          keyType = "SYMBOL",
+          OrgDb = "org.Mm.eg.db",
+          pvalueCutoff = 0.05
+     )
+
+     # Save ORA results to output file.
+     write.csv(
+          as.data.frame(go_res), 
+          paste0(getwd(), "/results/bulk/", comparison, "/", comparison, "_ORA_enrichGO_compareCluster_p0.05_results_table.csv")
+     )
+
+     go_res <- as.data.frame(go_res)
+
+     return(go_res)
+}
+
+make_dotplot <- function(
+     go_res, # Output of compareCluster.
+     title, # Title for bar plot.
+     terms # Terms to plot
+) {
+     plot <- ggplot(
+          go_res[go_res$Description %in% terms,],
+          aes(x = -log10(p.adjust), y = reorder(Description, -log10(p.adjust)), fill = Cluster, size = Count) 
+     ) +
+     geom_point(shape = 21, color = "black") +
+     theme_bw() +
+     theme(
+          axis.text = element_text(size = 10),
+          axis.title = element_text(size = 12),
+          plot.title = element_text(size = 12, face = "bold", hjust = 0.5)
+     ) +
+     xlab(expression(-log[10]~"(Adjusted p-value)")) +
+     ylab("Gene Ontology (GO) term") +
+     ggtitle(title) +
+     scale_fill_manual(
+          values = c("indianred1", "royalblue1"),
+          labels = c("Upregulated", "Downregulated"),
+          name = "Cluster"
+     ) +
+     scale_size_continuous(range = c(1,5), name = "# Genes") +
+     geom_vline(xintercept = -log10(0.05), linetype = "dashed")
+
+     return(plot)
+
+}
+
+
+#########################################
+#### Reading & processing input data ####
+#########################################
+
+#### HTKO vs WT ####
+
+# Read file.
+htko_df <- read.csv(
+     paste0(getwd(), "/results/bulk/HTKO_vs_WT/DESeq2_filtered_DEGs_pvalue0.05.csv"), 
+     header = TRUE,
+     row.names = NULL
+)
+
+# Perform over-representation analysis (ORA).
+htko_go <- process_degs(
+     deg_df = htko_df,
+     comparison = "HTKO_vs_WT"
+)
+
+# Select terms to plot.
+htko_terms_to_plot <- c(
+     "embryonic placenta development",
+     "reproductive system development",
+     "epithelial cell proliferation",
+     "cellular response to oxidative stress",
+     "nuclear transport", 
+     "regulation of G1/S transition Of mitotic cell cycle",
+     "stress-activated MAPK cascade",
+     "trophectodermal cell differentiation",
+     "lyase activity",
+     "secondary active transmembrane transporter activity",
+     "microtubule binding",
+     "symporter activity",
+     "tubulin binding",
+     "chaperone-mediated protein transport",
+     "negative regulation of DNA-binding transcription factor activity",
+     "mRNA processing",
+     "regulation of RNA splicing"
+)
+
+# Determine order of clusters.
+htko_go$Cluster <- factor(
+  htko_go$Cluster, 
+  levels = c('Upregulated', 'Downregulated') 
+)
+
+# Create plot with ORA results & save it to output file.
+htko_dotplot <- make_dotplot(
+     go_res = htko_go,
+     title = expression(STIP1^'+/-'~italic('versus')~'WT'),
+     terms = htko_terms_to_plot
+)
+pdf(paste0(getwd(), "/results/bulk/HTKO_vs_WT/HTKO_vs_WT_ORA_enrichGO_compareCluster_p0.05_selected_terms.pdf"), width = 10, height = 5)
+htko_dotplot
+dev.off()
+
+#### TGA vs WT ####
+
+# Read file.
+tga_df <- read.csv(
+     paste0(getwd(), "/results/bulk/TGA_vs_WT/DESeq2_filtered_DEGs_pvalue0.05.csv"), 
+     header = TRUE,
+     row.names = NULL
+)
+
+# Perform over-representation analysis (ORA).
+tga_go <- process_degs(
+     deg_df = tga_df,
+     comparison = "TGA_vs_WT"
+)
+
+# Select terms to plot.
+tga_terms_to_plot <- c(
+     "regulation of cell cycle arrest",
+     "regulation of apoptotic signaling pathway",
+     "regulation of cell cycle G1/S phase transition",
+     "signal transduction by p53 class mediator",
+     "cellular response to radiation",
+     "cellular response to hypoxia",
+     "endothelial cell migration",
+     "cellular response to UV",
+     "formation of primary germ layer",
+     "nuclear transport",
+     "cell fate commitment",
+     "signal transduction involved in mitotic G1 DNA damage checkpoint",
+     "negative regulation of cell development"
+)
+
+# Determine order of clusters.
+tga_go$Cluster <- factor(
+  tga_go$Cluster, 
+  levels = c('Upregulated', 'Downregulated') 
+)
+
+# Create plot with ORA results & save it to output file.
+tga_dotplot <- make_dotplot(
+     go_res = tga_go,
+     title = expression(STIP1^'TgA'~italic('versus')~'WT'),
+     terms = tga_terms_to_plot
+)
+pdf(paste0(getwd(), "/results/bulk/TGA_vs_WT/TGA_vs_WT_ORA_enrichGO_compareCluster_p0.05_selected_terms.pdf"), width = 10, height = 5)
+tga_dotplot
+dev.off()
+
+
+#### TGA vs HTKO ####
+
+# Read file.
+tga_df <- read.csv(
+     paste0(getwd(), "/results/bulk/TGA_vs_HTKO/DESeq2_filtered_DEGs_pvalue0.05.csv"), 
+     header = TRUE,
+     row.names = NULL
+)
+
+# Perform over-representation analysis (ORA).
+tga_go <- process_degs(
+     deg_df = tga_df,
+     comparison = "TGA_vs_HTKO"
+)
+
+# Select terms to plot.
+tga_terms_to_plot <- c(
+     "intrinsic apoptotic signaling pathway by p53 class mediator",
+     "signal transduction by p53 class mediator",
+     "intrinsic apoptotic signaling pathway in response to DNA damage by p53 class mediator",
+     "DNA damage response, signal transduction by p53 class mediator resulting in cell cycle arrest",
+     "positive regulation of apoptotic signaling pathway",
+     "regulation of intrinsic apoptotic signaling pathway",
+     "Wnt signaling pathway",
+     "regulation of cell cycle G1/S phase transition",
+     "placenta development",
+     "embryonic placenta development",
+     "neural tube development",
+     "endoderm development",
+     "muscle organ development",
+     "cell fate commitment",
+     "regulation of muscle cell differentiation"
+)
+
+# Determine order of clusters.
+tga_go$Cluster <- factor(
+  tga_go$Cluster, 
+  levels = c('Upregulated', 'Downregulated') 
+)
+
+# Create plot with ORA results & save it to output file.
+tga_dotplot <- make_dotplot(
+     go_res = tga_go,
+     title = expression(STIP1^'TgA'~italic('versus')~STIP1^'+/-'),
+     terms = tga_terms_to_plot
+)
+pdf(paste0(getwd(), "/results/bulk/TGA_vs_HTKO/TGA_vs_HTKO_ORA_enrichGO_compareCluster_p0.05_selected_terms.pdf"), width = 11.5, height = 5)
+tga_dotplot
+dev.off()
+
+
+#### TPR1 vs WT ####
+
+# Read file.
+tpr1_df <- read.csv(
+     paste0(getwd(), "/results/bulk/TPR1_vs_WT/DESeq2_filtered_DEGs_pvalue0.05.csv"), 
+     header = TRUE,
+     row.names = NULL
+)
+
+# Perform over-representation analysis (ORA).
+tpr1_go <- process_degs(
+     deg_df = tpr1_df,
+     comparison = "TPR1_vs_WT"
+)
+
+# Select terms to plot.
+tpr1_terms_to_plot <- c(
+     "cell cycle arrest",
+     "negative regulation of cell cycle",
+     "positive regulation of proteolysis",
+     "actin filament organization",
+     "regulation of apoptotic signaling pathway",
+     "negative regulation of nervous system development",
+     "DNA repair",
+     "regulation of cell morphogenesis involved in differentiation",
+     "proteasome-mediated ubiquitin-dependent protein catabolic process",
+     "Rho protein signal transduction",
+     "autophagy",
+     "epithelial tube morphogenesis",
+     "morphogenesis of a branching structure",
+     "cell fate commitment",
+     "DNA-binding transcription repressor activity",
+     "epithelial tube morphogenesis"
+)
+
+# Determine order of clusters.
+tpr1_go$Cluster <- factor(
+  tpr1_go$Cluster, 
+  levels = c('Upregulated', 'Downregulated') 
+)
+
+# Create plot with ORA results & save it to output file.
+tpr1_dotplot <- make_dotplot(
+     go_res = tpr1_go,
+     title = expression(STIP1^{Delta~'TPR1'}~italic('versus')~'WT'),
+     terms = tpr1_terms_to_plot
+)
+pdf(paste0(getwd(), "/results/bulk/TPR1_vs_WT/TPR1_vs_WT_ORA_enrichGO_compareCluster_p0.05_selected_terms.pdf"), width = 10, height = 5)
+tpr1_dotplot
+dev.off()
